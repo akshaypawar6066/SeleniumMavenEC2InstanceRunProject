e9d3792e0c969a4456fd840d1bd2b720bea26cd6
akshaypawar6066 <akshaypawar6066@gmail.com>
2024-01-25 01:39:55 +0530

    Changed EC2 IP

--- src/main/java/qa/DriverFactory.java
+++ src/main/java/qa/DriverFactory.java
@@ -29,7 +29,7 @@ public class DriverFactory {
 			capabilities = new FirefoxOptions();
 		}
 
-		driver = new RemoteWebDriver(new URL("http://13.201.28.151/:4444/wd/hub"), capabilities);
+		driver = new RemoteWebDriver(new URL("http://3.109.60.108:4444/wd/hub"), capabilities);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		return driver;
 
